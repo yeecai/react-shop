950d69c05fe9fcca8391ab6dde9c9a52e0a5f2c3
liliancai <yushunchn@gmail.com>
2019-03-26 14:32:01 +0100

    Updates

---
+++ precache-manifest.454e7f86916217aa99398348060b37ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-shop/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "de02d0693ead787c9df6",
+    "url": "/react-shop/static/js/runtime~main.d71eadc7.js"
+  },
+  {
+    "revision": "69c42553fff60a92984f",
+    "url": "/react-shop/static/js/main.e70d9bb2.chunk.js"
+  },
+  {
+    "revision": "ccdf85a272b151ebd001",
+    "url": "/react-shop/static/js/2.7b7f7f25.chunk.js"
+  },
+  {
+    "revision": "69c42553fff60a92984f",
+    "url": "/react-shop/static/css/main.2ac0cb14.chunk.css"
+  },
+  {
+    "revision": "0f524ee6152c82ebd4ef105eec8d1827",
+    "url": "/react-shop/index.html"
+  }
+];
