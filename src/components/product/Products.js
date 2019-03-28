a18fcde7b45b92d9838de1bea69d876e90a0446c
liliancai <yushunchn@gmail.com>
2019-03-28 10:46:50 +0100

    products2

--- src/components/product/Products.js
+++ src/components/product/Products.js
@@ -1,44 +1,59 @@
 import React, { Component } from 'react';
 import Paper from '@material-ui/core/Paper';
 import { Link } from 'react-router-dom';
+import { withStyles } from '@material-ui/core/styles';
+import PropTypes from 'prop-types';
+
 import config from '../../assets/store_config';
 import Grid from '@material-ui/core/Grid';
 import test from '/home/lilian/react/react-shop/src/assets/p1/IMG_20190215_152601.jpg';
 {/*import test2 from './IMG_20180807_164722.jpg'*/}
-const numbers = [1, 2, 3, 4, 5];
 
-
-
-
-const styles = ({
+const styles = theme =>({
     pic:{
         padding:'0',
         width:"100%",
         height:"100%"
     },
+    product:{
+        padding: theme.spacing.unit* 2,
+        position:'relative',
+
+    },
     products:{
-        display: 'grid',
-        margin: '100px 20px 100px 10px',
+        position:'relative',
+        padding:'10px',
+        flexGrow: 1,
+        textDecoration: 'none',
+        margin: '9px 19px 9px 9px',
     },
     title:{
         textDecoration: 'none',
+        color:'black'
     }
 });
-const listItems = config.productsinfo.map((each) =>
-    <Grid item xs={6}>
-        <Link to="#" style={styles.title}></Link>
-        <img src={require(`../../assets/${each.photos[0]}`)} style ={styles.pic}/>
-        <div style={styles.title}>{each.name}</div>
-    </Grid>
 
-  );
 class Products extends Component{
-
     render(){
+        const {classes} = this.props
+        const listItems = config.productsinfo.map((each) =>
+        <Grid item xs={6}>
+            <Paper className={classes.product}>
+                <Link to="#" className ={classes.title}>
+                    <img src={require(`../../assets/${each.photos[0]}`)} alt={each.name} className ={classes.pic}/>
+                    <div>{each.name}</div>
+                    <div>{each.price}</div>
+                </Link>
+            </Paper>
+        </Grid>
+
+          );
         return(
-            <Grid container spacing={12}>{listItems}</Grid>
+            <div className={classes.products}>
+                <Grid container spacing={24} >{listItems}</Grid>
+            </div>
         );
     }
 };
 
-export default Products;
+export default withStyles(styles)(Products);
