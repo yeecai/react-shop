ee0eed7f6471ae806733deaf458b6261de31006e
liliancai <yushunchn@gmail.com>
2019-03-29 21:21:40 +0100

    displayed all photos in product-page

--- src/components/product/Product.js
+++ src/components/product/Product.js
@@ -4,15 +4,20 @@ import { Route, Link, Redirect } from 'react-router-dom'
 class ProductDetailPage extends Component{
     render(){
         const { me } = this.props;
+        {/*alert(me.photos);*/}
+        const photoss = me.photos;
+        const photoList = photoss.map((each)=>
+            <li><img src={require(`../../assets/${each}`)}/></li>
+        );
         return(
-        <div style={{padding:'200px'}}>
-            <h1>All stuff</h1>
-            <h2>Giant test heading</h2>
-            <div></div>
-            <Link to="/react-shop">I'm {me.name}</Link>
-        </div>
-    )
-}
+            <div style={{padding:'200px'}}>
+                <h1>All stuff</h1>
+                <h2>Giant test heading</h2>
+                <ul>{photoList}</ul>
+                <Link to="/react-shop">I'm {me.name}</Link>
+            </div>
+        );
+    }
 }
 
 export default ProductDetailPage;
