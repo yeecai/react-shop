fccc67974ed81566542ff28ace80011d7475abd0
liliancai <yushunchn@gmail.com>
2019-04-02 10:49:57 +0200

    obejct-fit:cover squared the images in cart

--- src/components/cart/Cart.js
+++ src/components/cart/Cart.js
@@ -1,4 +1,28 @@
 import React, {Component} from 'react';
+import styled from 'styled-components';
+
+const Pic = styled.img`
+    // background-image: url("https://www.insidescience.org/sites/default/files/sites/default/files/images/articles/top-images/2018/5_heic1808a_crop.jpg");
+    // background:url("/static/media/00.4e34cd2c.jpg"); // This one works!!
+    // background-image: url(${props=>props.img})
+    // background:url("/home/lilian/react/react-shop/src/assets/p1/00.jpg");
+    // why those not working except https!!!
+    //background-image: url("../../assets/p1/00.jpg");
+//    background-image: url(00.jpg);
+// never mind, url() never work for me - - !
+    padding:0;
+    width:125px;
+    height:125px;
+    object-fit: cover; // same square effect!
+}
+`;
+const Table =styled.table`
+
+    thead > tr > th{
+        font-weight: normal;
+        text-align: left;
+    }
+`;
 
 class Cart extends Component {
     constructor(props){
@@ -19,15 +43,17 @@ class Cart extends Component {
         return (
             <div>
             <h1>Cart</h1>
-            <table>
-            <thead>Okay I may missed sth, the images are broken.</thead>
-            <tbody>
-            {this.state.items.map((each) =>
-                    <tr><td><img src={require(`../../assets/${each.url}/${each.pic}`)}/></td></tr>
-                )
-            }
-            </tbody>
-            </table>
+            <Table>
+                <thead><tr><th>Product</th><th>Amount</th><th>Total</th><th></th></tr></thead>
+                <tbody>
+                {this.state.items.map((each) =>
+                        <tr><td><Pic src={require(`../../assets/${each.url}/${each.pic}`)}/></td><td>{each.name}</td></tr>
+                        // <tr><td><Pic/></td><td>{each.name}</td></tr>
+
+                    )
+                }
+                </tbody>
+            </Table>
             </div>
             )
     }
@@ -39,4 +65,6 @@ export default Cart;
 //<img src=require({"../home/lilian/react/react-shop/src/assets/p1/00.jpg"})/>
 //<p>{each.url}/{each.pic}</p>
 //<img src="/static/media/00.4e34cd2c.jpg"/>*/}
-{/*after all those tries!!!turns out its my browser's problem!!!*/}
+{/*after all those tries!!!turns out its my browser's problem!!!
+{`../../assets/${each.url}/${each.pic}`}
+    */}
